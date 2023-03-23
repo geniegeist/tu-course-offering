bcdb7922efa7a8b48ebed9ea42d59c2286660a3f
Viet Duc Nguyen <vd.nguyen96@gmail.com>
2023-03-23 10:00:44 +0100

    Enable analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 import Head from 'next/head';
 import ThemeProvider from '../theme';
 
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
